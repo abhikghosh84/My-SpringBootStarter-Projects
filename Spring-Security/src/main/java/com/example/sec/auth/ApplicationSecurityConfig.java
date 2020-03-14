65c2f1ec78a13ea48216b7dc5023e058029e71b7
abhik <abhik@LAPTOP-QFA8U9SF>
2020-03-14 21:55:11 +0530

    adding Security configuration

---
+++ Spring-Security/src/main/java/com/example/sec/auth/ApplicationSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.sec.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable();
+
+		http
+		.authorizeRequests()
+		.antMatchers("/public/**").permitAll()
+		.and()
+		.httpBasic();
+
+	}
+
+}

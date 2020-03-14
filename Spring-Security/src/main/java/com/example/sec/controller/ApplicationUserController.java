47f1b8bc47225df0e2ee89e58ffc4c100a990523
abhik <abhik@LAPTOP-QFA8U9SF>
2020-03-15 00:39:25 +0530

    configured H2

---
+++ Spring-Security/src/main/java/com/example/sec/controller/ApplicationUserController.java
@@ -0,0 +1,11 @@
+package com.example.sec.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secured/user")
+public class ApplicationUserController {
+
+	
+}

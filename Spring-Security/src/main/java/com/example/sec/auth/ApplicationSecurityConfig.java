fe9495c652c34b5314065abfb7b7897ef975bdd6
abhik <abhik@LAPTOP-QFA8U9SF>
2020-03-14 22:58:36 +0530

    Basic Authorization

--- Spring-Security/src/main/java/com/example/sec/auth/ApplicationSecurityConfig.java
+++ Spring-Security/src/main/java/com/example/sec/auth/ApplicationSecurityConfig.java
@@ -1,12 +1,26 @@
 package com.example.sec.auth;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("derby").password(appPasswordEncoder().encode("test").toString()).roles("IT_ADMIN");
+		auth.inMemoryAuthentication().withUser("tom").password(appPasswordEncoder().encode("test").toString()).roles("TRAINEE");
+	}
+	
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 
@@ -19,6 +33,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 		.httpBasic();
 
+	}
+	
+	@Bean
+	public PasswordEncoder appPasswordEncoder() {
+		BCryptPasswordEncoder p = new BCryptPasswordEncoder(10);
+		return p;
 	}
 
 }
--- Spring-Security/src/main/java/com/example/sec/controller/SecuredController.java
+++ Spring-Security/src/main/java/com/example/sec/controller/SecuredController.java
@@ -1,5 +1,6 @@
 package com.example.sec.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,21 +16,25 @@ import com.example.sec.model.Greeting;
 @RequestMapping("/secured/api")
 public class SecuredController {
 	@GetMapping
+	@PreAuthorize(value = "hasAnyRole('IT_ADMIN', 'TRAINEE')")
 	public String sayGreeting() {
 		return "Hello from Spring Starter Project - Secured";
 	}
 
 	@PostMapping
+	@PreAuthorize(value = "hasRole('IT_ADMIN')")
 	public String addGreeting(@RequestBody Greeting greeting) {
 		return greeting + " is added to the db!";
 	}
 
 	@PutMapping
+	@PreAuthorize(value = "hasRole('IT_ADMIN')")
 	public String updateGreeting(@RequestBody Greeting greeting) {
 		return greeting + " is updated in the db!";
 	}
 
 	@DeleteMapping
+	@PreAuthorize(value = "hasRole('IT_ADMIN')")
 	public String deleteGreeting(@RequestParam Integer greetingId) {
 		return greetingId + " is deleted from the db!";
 	}

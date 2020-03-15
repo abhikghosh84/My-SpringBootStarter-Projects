02d15c0569c124aae45f40e3e7c06faed860a97d
abhik <avik5324@gmail.com>
2020-03-15 19:36:43 +0530

    modified

---
+++ Spring-Security-Jwt/src/main/java/com/example/sec/auth/CustomUserDetails.java
@@ -0,0 +1,77 @@
+package com.example.sec.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.sec.model.ApplicationUser;
+
+public class CustomUserDetails implements UserDetails{
+	
+	private ApplicationUser user;
+	
+	
+
+	public ApplicationUser getUser() {
+		return user;
+	}
+
+	public void setUser(ApplicationUser user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_"+user.getRole().getRole()));
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return user.getActive().equalsIgnoreCase("Y")? true : false;
+	}
+
+	@Override
+	public String toString() {
+		return "CustomUserDetails [getUser()=" + getUser() + ", getAuthorities()=" + getAuthorities()
+				+ ", getPassword()=" + getPassword() + ", getUsername()=" + getUsername() + ", isAccountNonExpired()="
+				+ isAccountNonExpired() + ", isAccountNonLocked()=" + isAccountNonLocked()
+				+ ", isCredentialsNonExpired()=" + isCredentialsNonExpired() + ", isEnabled()=" + isEnabled() + "]";
+	}
+	
+	
+
+}
